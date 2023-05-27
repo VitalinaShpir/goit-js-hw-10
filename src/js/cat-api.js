5d6a19711f27ea6aacb23f6adcf7fed8995a140d
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-28 00:46:42 +0300

    sdc vdcfv

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,4 +1,5 @@
+const API =
+  'live_0P4LYrUD2RzxWsDzIlhzu1TTwJUDuhYwNccomvc5O50B6yfZQVjWwUSTbk8vvIaw';
 
 
-
-console.log('hello!!!111')
+console.log('hello!!!111');
--- src/js/index.js
+++ src/js/index.js
@@ -3,120 +3,99 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash';
 // import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-const DEBOUNCE_DELAY = 300;
+const API = "live_0P4LYrUD2RzxWsDzIlhzu1TTwJUDuhYwNccomvc5O50B6yfZQVjWwUSTbk8vvIaw";
 
 const breedSelect = document.querySelector('.breed-select');
 const loaderRef = document.querySelector('.loader');
 const errorRef = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+const catImg = document.querySelector('.cat-img');
+const catDescr = document.querySelector('.cat-descr');
 
-console.log('hello!!2222')
-
+console.log('hello!!21111222')
 
  function fetchBreeds() {
-    return fetch('https://api.thecatapi.com/v1/breeds')
-      .then(response => response.json())
-      .then(data => data)
-      .catch(error => {
-        throw error;
-      });
+    return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API}`).then(
+      response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      }
+    );
   }
   
-   function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-      .then(response => response.json())
-      .then(data => data)
+   function fetchCatByBreed(breed) {
+    return fetch(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}&x-api-key=${API}`
+    ).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      
+      return response.json();
+    });
+  }
+console.log(fetchCatByBreed('acur'))
+
+//   -----------------------------------------------
+
+
+function openBreeds() {
+    fetchBreeds()
+      .then(breeds => {
+        breeds.forEach(breed => {
+          const option = document.createElement('option');
+          option.value = breed.id;
+          option.textContent = breed.name;
+          breedSelect.appendChild(option);
+          loaderRef.style.display = 'block';
+        })
+      })
       .catch(error => {
-        throw error;
+        breedSelect.style.display = 'block';
+        loaderRef.style.display = 'none';
+        errorRef.style.display = 'block';
+        console.error(error);
       });
+  }
+  openBreeds();
+
+
+//   ----------------------------------------------
+
+
+breedSelect.addEventListener('change', onBreedCange);
+
+function onBreedCange(evt){
+    loaderRef.style.display = 'block';
+  catImg.innerHTML = '';
+  catDescr.innerHTML = '';
+  const breed = evt.target.value;
+  console.log(breed);
+  fetchCatByBreed(breed)
+    .then(breed => renderCatCard(breed))
+    .catch(error => Notiflix.Notify.failure( 'Oops! Something went wrong! Try reloading the page!'))
+    .finally(() => loaderRef.style.display = 'block');
+}
+
+
+
+// ----------------------------------------------
+
+
+
+
+//   -----------------------------------------------
+
+
+function renderCatCard (breed){
+    const markupImg = `<img class="cat-picture" width=400 src="${breed.url}" alt="bbb">`;
+    // const markupDescr = `<h1 class="cat-name">${breed.breeds[0].name}</h2><p class="cat-description
+    // ">${breed.breeds[0].description}</p><p class="cat-temperament"><b>Temperament:</b> ${breed.breeds[0].temperament}</p>`;
+    catInfo.insertAdjacentHTML('beforeend', markupImg);
+    // catInfo.insertAdjacentHTML('beforeend', markupDescr);
   }
 
 
-function showLoader() {
-  loaderRef.style.display = 'block';
-}
-
-function hideLoader() {
-  loaderRef.style.display = 'none';
-}
-
-function openBreeds() {
-  showLoader();
-  fetchBreeds()
-    .then(breeds => {
-      breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-      });
-
-      hideLoader();
-      breedSelect.style.display = 'block';
-    })
-    .catch(error => {
-      hideLoader();
-      showError();
-      console.error(error);
-    });
-}
-openBreeds();
-
-
-function fetchCatInfo(breedId) {
-  showLoader();
-  hideCatInfo();
-
-  fetchCatByBreed(breedId)
-    .then(cats => {
-      if (cats.length > 0) {
-        const cat = cats[0];
-        renderCatCard(cat);
-      } else {
-        throw new Error('No cat found for the selected breed.');
-      }
-
-      hideLoader();
-    })
-    .catch(error => {
-      hideLoader();
-      showError();
-      console.error(error);
-    });
-}
-
-breedSelect.addEventListener('change', evt => {
-  const selectedBreedId = evt.target.value;
-  fetchCatInfo(selectedBreedId);
-});
-
-function hideCatInfo() {
-  catInfo.style.display = 'none';
-}
-
-
-// function renderCatCard (events){
-//     const markup = events
-//     .map(({ name, url, breeds }) => {
-//       return `  <div><img class="cat-img" src="${url}" alt="${name}">
-//     <div class="descr-box">
-//     <h1 class="title">${breeds[0].name}</h1>
-//     <p class="description">${breeds[0].description}</p>
-//     <p class="temperament">${breeds[0].temperament}</p>
-// </div></div>' alt='${name}' width='200'></li>`;
-//     })
-//     .join("");
-// catInfo.insertAdjacentHTML("beforeend", markup);
-// }
-// console.log(renderCatCard (events))
-
-
-
-function showError() {
-  errorRef.style.display = 'block';
-}
-
-function hideError() {
-  errorRef.style.display = 'none';
-}
-
+  
