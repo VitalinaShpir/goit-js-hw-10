4d5c9e49edeb5ba43891b70d3e3f5497011e7ddc
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-28 21:32:21 +0300

    add cat-api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,7 +1,25 @@
+const API =
+  'live_HEs7npt4enTv8IppoFAzotzjElNW9aw61wQB5T2Fw18DPSakhIju9elgFzOgYqmc';
 
 
+    const fetchBreeds = () => {
+  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
 
-    
+
+    const fetchCatByBreed = breedId => {
+  return fetch(`https://api.thecatapi.com/v1/images/${breedId}?api_key=${API}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
 
 
-// export { fetchBreeds, fetchCatByBreed };
+export { fetchBreeds, fetchCatByBreed };
--- src/js/index.js
+++ src/js/index.js
@@ -16,28 +16,7 @@ breedSelect.addEventListener('change', onBreedChange);
 
 Notiflix.Loading.circle('Loading data, please wait...');
 
-const API =
-  'live_HEs7npt4enTv8IppoFAzotzjElNW9aw61wQB5T2Fw18DPSakhIju9elgFzOgYqmc';
 
-
-    const fetchBreeds = () => {
-  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
-
-
-    const fetchCatByBreed = breedId => {
-  return fetch(`https://api.thecatapi.com/v1/images/${breedId}?api_key=${API}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
  
   fetchBreeds()
       .then(breeds => {
