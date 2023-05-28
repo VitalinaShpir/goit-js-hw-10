2d86994cd3e4090c7c5b859f2fc559b29d8856d8
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-28 21:46:53 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import '../css/styles.css';
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
